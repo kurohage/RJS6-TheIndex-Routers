637899135814372930bfa151a245cf3207369a66
kurohage <517326+kurohage@users.noreply.github.com>
2019-11-08 01:16:38 +0300

    Task completed.

--- src/BookList.js
+++ src/BookList.js
@@ -5,30 +5,17 @@ import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import BookRow from "./BookRow";
 
-class BookList extends Component {
+export default class BookList extends Component {
   state = {
     filteredBooks: this.props.books
   };
 
   filterBooks = query => {
-    let condition;
-    if (this.props.match.params.bookColor === undefined) condition = "title";
-    else condition = "color";
-
     let filteredBooks = this.props.books.filter(book =>
       `${book.title}`.toLowerCase().includes(query.toLowerCase())
     );
 
     this.setState({ filteredBooks: filteredBooks });
-  };
-
-  filterBooksByColor = () => {
-    let query = this.props.match.params.bookColor.toLowerCase();
-    console.log("Query for books: ", query);
-    let books = this.props.books.filter(book =>
-      book.color.toLowerCase().includes(query.toLowerCase())
-    );
-    this.setState({ filteredBooks: books });
   };
 
   render() {
@@ -39,10 +26,12 @@ class BookList extends Component {
         <BookRow key={book.id} book={book} authors={book.authors} />
       ));
     } else {
-      bookCards = this.state.filteredBooks.filter(book =>
-        (book.color == this.props.match.params.bookColor).map(book => {
-          return <BookRow key={book.id} book={book} authors={book.authors} />;
-        })
+      bookCards = this.state.filteredBooks.map(book =>
+        book.color === this.props.match.params.bookColor ? (
+          <BookRow key={book.id} book={book} authors={book.authors} />
+        ) : (
+          false
+        )
       );
     }
 
@@ -64,5 +53,3 @@ class BookList extends Component {
     );
   }
 }
-
-export default BookList;
